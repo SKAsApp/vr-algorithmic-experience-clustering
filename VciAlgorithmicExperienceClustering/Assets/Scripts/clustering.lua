f4b9f0699c9a14d7046cb7e6cef48e1bfa7a0dee
SKAsApp <60283197+SKAsApp@users.noreply.github.com>
2020-08-16 16:50:07 +0900

    culsterクラスたぶん完成

--- VciAlgorithmicExperienceClustering/Assets/Scripts/clustering.lua
+++ VciAlgorithmicExperienceClustering/Assets/Scripts/clustering.lua
@@ -1,19 +1,24 @@
 local Data = require "data"
+local function_tools = require "function_tools"
 
--- Clusteringクラス
--- 	k-meansの計算をします。
+-- Clusteringクラス（？）
+-- 	k-means計算機
 local Clustering = { }
 Clustering.new = function(positions, k)
 	-- param positions: データの座標のリスト
 	-- param k: クラスター数k
 	local instance = { }
 	instance.__positions = positions
-	instance.__length = #datas
-	-- datasはDataインスタンスのリスト
+	instance.__length = #positions
 	instance.__datas = { }
 	instance.__k = k
+	instance.__centers = { }
 
-	instance.__initialize = function( )
+	instance.get_datas = function( )
+		return instance.__datas
+	end
+
+	instance.initialize = function( )
 		-- テキトーにクラスター番号を割り振ります。
 		local separation = math.floor(instance.__length / instance.__k)
 		for i = 0, separation do
@@ -25,9 +30,65 @@ Clustering.new = function(positions, k)
 		for i = separation * 2 + 1, instance.__length do
 			instance.datas[i] = Data.new(instance.__positions[i], 2)
 		end
+	end
 
-	instance.get_position = function( )
-		return instance.__position
+	instance.k_means = function( )
+		-- クラスタリングの計算をします。
+		instance.__calculate_centers( )
+		instance.__re_calculate_cluster( )
+	end
+
+	instance.__calculate_centers = function( )
+		-- すべてのクラスターの重心を計算します。
+		for i = 0, instance.__k - 1 do
+			instance.__calculate_center(i)
+		end
+	end
+
+	instance.__calculate_center = function(i)
+		-- 特定のクラスターの重心を計算します。
+		-- param i: 計算するクラスター番号
+		-- i番クラスターに属すデータのみ抽出
+		local cluster_i = function_tools.filter_keep_key(function(data) return data:get_cluster( ) == i end, instance.__datas)
+		-- 座標データのみ抽出
+		local positions_i = { }
+		for key, value in pairs(cluster_i) do
+			table.insert(positions_i, value:get_position( ))
+		end
+		-- 重心を更新
+		instance.__centers[i] = function_tools.average(positions_i)
+	end
+
+	instance.__re_calculate_cluster = function( )
+		-- クラスターを再割当てします。
+		-- すべてのデータiに対して，それぞれのクラスターjとの距離を計算 → 距離が1番近いクラスターに再割当て
+		for i, data in pairs(instance.__datas) do
+			local distances = instance.__calculate_data_clusters_distance(i, data)
+			local minimum_distance = function_tools.minimum_absolute(distances)
+			instance.__update_new_cluster(i, minimum_distance)
+		end
+	end
+
+	instance.__calculate_data_clusters_distance = function(i, data)
+		-- データiとそれぞれのクラスターjとの距離を計算します。
+		-- param i: とあるデータのインデックス
+		-- param data: i番目のデータ
+		-- return: 距離配列（key＝クラスター番号，value＝距離）
+		local distances = { }
+		for j, center in pairs(instance.__centers) do
+			distances[j] = Vector3.Distance(data:get_position( ), center)
+		end
+		return distances
+	end
+
+	instance.__update_new_cluster = function(i, minimum_distance)
+		-- データインスタンスを新しいクラスター番号に更新します。
+		-- param i: とあるデータのインデックス
+		-- param minimum_distance: 最小の距離のインデックスと距離の入ったテーブル
+		local previous_cluster = instance.__datas[i]:get_cluster( )
+		local now_cluster = minimum_distance["index"]
+		instance.__datas[i]:set_previous_cluster(previous_cluster)
+		instance.__datas[i]:set_cluster(now_cluster)
 	end
 
 end
--- VciAlgorithmicExperienceClustering/Assets/Scripts/data.lua
+++ VciAlgorithmicExperienceClustering/Assets/Scripts/data.lua
@@ -1,4 +1,4 @@
--- Dataクラス
+-- Dataクラス（？）
 -- 	座標とクラスター番号を保持
 local Data = { }
 Data.new = function(position, cluster)
@@ -7,6 +7,7 @@ Data.new = function(position, cluster)
 	local instance = { }
 	instance.__position = position
 	instance.__cluster = cluster
+	instance.__previous_cluster = 0
 
 	instance.get_position = function( )
 		return instance.__position
@@ -14,6 +15,18 @@ Data.new = function(position, cluster)
 
 	instance.get_cluster = function( )
 		return instance.__cluster
+	end
+
+	instance.set_cluster = function(value)
+		instance.__cluster = value
+	end
+
+	instance.get_previous_cluster = function( )
+		return instance.__previous_cluster
+	end
+
+	instance.set_previous_cluster = function(value)
+		instance.__previous_cluster = value
 	end
 
 end
